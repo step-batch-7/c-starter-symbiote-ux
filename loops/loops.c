7772a04cf67704952530832bd7ba89467ea20edd
Saurabh <58027263+symbiote-ux@users.noreply.github.com>
2020-03-29 09:50:57 +0530

    print_fibonacci upto N terms function added

--- loops/loops.c
+++ loops/loops.c
@@ -1,6 +1,21 @@
 #include<stdio.h>
 
 int find_factorial(int);
+int print_fibonacci(int);
+
+int print_fibonacci(int num) {
+  int prev_term = -1;
+  int next_term = 1;
+  int count = 0;
+  while(count != num){
+  int curr_term = prev_term + next_term;
+  prev_term = next_term;
+  next_term = curr_term;
+  printf("%d \n",curr_term);
+  count++;
+  }
+  return 0;
+}
 
 int find_factorail(int num) {
   if(num == 0) return 1;
@@ -9,8 +24,13 @@ int find_factorail(int num) {
 
 int main() {
   int num;
-  printf("Enter a number to find factorail \n");
+  printf("Enter a number to find factorial \n");
   scanf("%d", &num);
   printf("Factorial of %d is %d \n",num, find_factorail(num));
+  int count;
+  printf("Enter number of terms you want to print of Fibonacci series \n");
+  scanf("%d",&count);
+  printf("Fibonacci series upto %d terms is : \n",count);
+  print_fibonacci(count);
   return 0;
 }
