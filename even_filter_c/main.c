a642e1fc6b79c948ab7c9f31393ce85c3ceccf82
Saurabh <58027263+symbiote-ux@users.noreply.github.com>
2020-04-16 17:49:54 +0530

    freed pointers

--- even_filter_c/main.c
+++ even_filter_c/main.c
@@ -19,7 +19,9 @@ int main() {
   for(int i = 0; i < even_num->length; i++) {
     printf("%d \n",even_num->list[i]);
   }
+  free(even_num->list);
   free(even_num);
+  free(all_num->list);
   free(all_num);
   return 0;
 }
